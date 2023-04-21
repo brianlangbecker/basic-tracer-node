fd5ec76d89ab8df743f758ba97e58fe416d18d73
brianlangbecker <brianlangbecker@hotmail.com>
2023-04-21 09:49:14 -0600

    Quick fix

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-'use strict';
+"use strict";
 // Import tracing before any other package is imported.
 require("./tracing.js");
 
-const opentelemetry = require('@opentelemetry/api');
-const tracer = opentelemetry.trace.getTracer('example-basic-tracer-node');
+const opentelemetry = require("@opentelemetry/api");
+const tracer = opentelemetry.trace.getTracer("example-basic-tracer-node");
 
 const firstSpan = tracer.startSpan("start");
-  firstSpan.setAttribute("number", 1);
-  const result = // and they set the result
-  firstSpan.end();
+firstSpan.setAttribute("number", 1);
+const result = firstSpan.end(); // and they set the result
 
 for (let i = 0; i < 10000; i += 1) {
-  tracer.startActiveSpan('main', (span) => {
+  tracer.startActiveSpan("main", (span) => {
     for (let k = 0; k < 100; k += 1) {
       doWork();
     }
 
     // Be sure to end the span!
     span.end();
-  
-  })
-};
-
-traceExporter.shutdown();
+  });
+}
 
 function doWork() {
   // simulate some random work.
