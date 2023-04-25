a207db4e0cc1d899b014bc2de22220f5d01f51dc
brianlangbecker <brianlangbecker@hotmail.com>
2023-04-25 10:24:55 -0600

    Fine tuning

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 "use strict";
 // Import tracing before any other package is imported.
 require("./tracing.js");
-
+const express = require("express");
 const opentelemetry = require("@opentelemetry/api");
 const tracer = opentelemetry.trace.getTracer("example-basic-tracer-node");
 
-const firstSpan = tracer.startSpan("start");
-firstSpan.setAttribute("number", 1);
-const result = firstSpan.end(); // and they set the result
+// App
+const app = express();
+app.get("/", async (req, res) => {
+  const firstSpan = tracer.startSpan("start");
+  firstSpan.setAttribute("number", 1);
+  const result = firstSpan.end(); // and they set the result
 
-for (let i = 0; i < 10000; i += 1) {
-  tracer.startActiveSpan("main", (span) => {
-    for (let k = 0; k < 100; k += 1) {
-      doWork();
-    }
+  for (let i = 0; i < 10; i += 1) {
+    tracer.startActiveSpan("main", (span) => {
+      for (let k = 0; k < 10; k += 1) {
+        doWork();
+      }
 
-    // Be sure to end the span!
-    span.end();
-  });
-}
-
-function doWork() {
-  // simulate some random work.
-  for (let j = 0; j <= Math.floor(Math.random() * 40000000); j += 1) {
-    // empty
-    //console.log("DoWork")
+      // Be sure to end the span!
+      span.end();
+    });
   }
-}
+
+  function doWork() {
+    // simulate some random work.
+    for (let j = 0; j <= Math.floor(Math.random() * 40000000); j += 1) {
+      // empty
+      //console.log("DoWork")
+    }
+  }
+
+  res.send("Hello World");
+});
+
+// Run API.
+app.listen(process.env.PORT || 3000, () => console.log("Listening on port 3000."));
--- tracing.js
+++ tracing.js
@@ -1,18 +1,22 @@
-const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-
-const { diag } = require("@opentelemetry/api")
-const { DiagConsoleLogger } =  require("@opentelemetry/api")
-const { DiagLogLevel } =  require("@opentelemetry/api")
-
+// Configure your .env with Honeycomb specific variables. See .env.sample.
+require("dotenv").config();
+const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const { Metadata, credentials } = require("@grpc/grpc-js");
+const { NodeSDK } = require("@opentelemetry/sdk-node");
+// const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+const { ExpressInstrumentation } = require("@opentelemetry/instrumentation-express");
+//const {OTLPTraceExporter} = require("@opentelemetry/exporter-trace-otlp-proto");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-grpc");
+// const { OTLPTraceExporter } = require("@opentelemetry/exporter-collector-grpc");
+const { diag } = require("@opentelemetry/api");
+const { DiagConsoleLogger } = require("@opentelemetry/api");
+const { DiagLogLevel } = require("@opentelemetry/api");
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-const traceExporter = new ConsoleSpanExporter();
+const traceExporter = new OTLPTraceExporter();
 const sdk = new NodeSDK({
   traceExporter,
-  instrumentations: [getNodeAutoInstrumentations()]
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
-sdk
-  .start()
+sdk.start();
